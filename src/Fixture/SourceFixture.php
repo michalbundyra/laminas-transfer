e9f6558781b1ec7dd54ce062e0dbd657f3d869f5
webimpress <contact@webimpress.com>
2019-03-06 22:11:03 +0000

    Disable printing file list in SourceFixture

--- src/Fixture/SourceFixture.php
+++ src/Fixture/SourceFixture.php
@@ -12,7 +12,6 @@ use function file_put_contents;
 use function getcwd;
 use function implode;
 use function system;
-use function var_export;
 
 /**
  * Change namespace and all references to ZF in source/test files
@@ -23,7 +22,6 @@ class SourceFixture extends AbstractFixture
     public function process(Repository $repository) : void
     {
         $phps = $repository->files('*.php');
-        $this->writeln(var_export($phps, true));
         foreach ($phps as $php) {
             $this->replace($repository, $php);
         }

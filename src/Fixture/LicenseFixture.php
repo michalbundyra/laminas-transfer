fe6f8acd8b4de34fb21523fe6414c95c624e9029
webimpress <contact@webimpress.com>
2019-03-29 14:28:47 +0000

    Replace whole LICENSE text (just in case it was not consistent somewhere)

--- src/Fixture/LicenseFixture.php
+++ src/Fixture/LicenseFixture.php
@@ -30,15 +30,42 @@ class LicenseFixture extends AbstractFixture
  */
 EOS;
 
+    private const LICENSE = <<<'LICENSE'
+Copyright (c) %u, Laminas Foundation
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+- Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+- Redistributions in binary form must reproduce the above copyright notice, this
+  list of conditions and the following disclaimer in the documentation and/or
+  other materials provided with the distribution.
+
+- Neither the name of Laminas Foundation nor the names of its contributors may
+  be used to endorse or promote products derived from this software without
+  specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+LICENSE;
+
     public function process(Repository $repository) : void
     {
         $license = current($repository->files('LICENSE.md'));
-
         if ($license) {
-            $content = file_get_contents($license);
-            $content = preg_replace('/(\d{4}-)?\d{4}+/', date('Y'), $content);
-            $content = $repository->replace($content);
-            file_put_contents($license, $content);
+            file_put_contents($license, sprintf(self::LICENSE, date('Y')));
         }
 
         $phps = $repository->files('*.php');
@@ -48,7 +75,8 @@ EOS;
 
         file_put_contents(
             $repository->getPath() . '/COPYRIGHT.md',
-            'Copyright (c) ' . date('Y') . ', Laminas. All rights reserved. (https://laminas.dev/)'
+            'Copyright (c) ' . date('Y') . ', Laminas Foundation.'
+                . "\n" . 'All rights reserved. (https://getlaminas.org/)'
         );
         system('git add ' . $repository->getPath() . '/COPYRIGHT.md');
 

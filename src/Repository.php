f2aedaff7db2fd24c5b8eb9461c912c5b681c99e
webimpress <contact@webimpress.com>
2019-04-02 22:41:28 +0100

    Better rewrite - using bridge library - added more replacement pairs
    Now rewrite should work with zend/expressive/apigility components

--- src/Repository.php
+++ src/Repository.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Laminas\Transfer;
 
+use Laminas\ZendFrameworkBridge\RewriteRules;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
 use RegexIterator;
@@ -19,17 +20,60 @@ class Repository
 {
     /** @var string[] */
     private $replacements = [
+        'ZendXml;' => 'Laminas\\Xml;',
+        'ZendXmlTest' => 'LaminasTest\\Xml',
+        'ZendXml' => 'laminas-xml',
+        'zendxml' => 'laminas-xml',
+        'ZendOAuth;' => 'Laminas\\OAuth;',
+        'ZendOAuthTest' => 'LaminasTest\\OAuth',
+        'ZendOAuth' => 'laminas-oauth',
+        'zendoauth' => 'laminas-oauth',
+        'ZendDiagnostics;' => 'Laminas\\Diagnostics;',
+        'ZendDiagnosticsTest' => 'LaminasTest\\Diagnostics',
+        'ZendDiagnostics' => 'zend-diagnostics',
+        'zenddiagnostics' => 'zend-diagnostics',
+        'ZendService_Amazon' => 'laminas-amazon',
+        'ZendService_Apple_Apns' => 'laminas-apple-apns',
+        'ZendService_Google_Gcm' => 'laminas-google-gcm',
+        'ZendService_Twitter' => 'laminas-twitter',
+        'zendservice-' => 'laminas-',
+        'zfcampus/zf-development-mode' => 'laminas/laminas-development-mode',
+        'zf-development-mode' => 'laminas-development-mode',
+        'zfcampus/zf-deploy' => 'laminas/laminas-deploy',
+        'zf-deploy' => 'laminas-deploy',
+        // @todo: need to determine the final name for zf-console
+        // 'zfcampus/zf-console' => 'laminas/laminas-console-application',
+        // 'zf-console' => 'laminas-console-application',
+        'zfcampus/zf-composer-autoloading' => 'laminas/laminas-composer-autoloading',
+        'zf-composer-autoloading' => 'laminas-composer-autoloading',
+        'zfcampus/zf-apigility' => 'apigility/apigility',
+        'zfcampis/zf-' => 'apigility/apigility',
+        'zfcampus/' => 'apigility/',
+        'zend-problem-details' => 'expressive-problem-details',
+        'zendframework/zend-expressive' => 'expressive/expressive',
+        'zend-expressive' => 'expressive',
+        // 'ZFTest\\Console' => 'LaminasTest\\ConsoleApplication',
+        'ZendTest\\ProblemDetails' => 'ExpressiveTest\\ProblemDetails',
+        'ZendTest\\Expressive' => 'ExpressiveTest',
+        'ZFTest\\ComposerAutoloading' => 'LaminasTest\\ComposerAutoloading',
+        'ZFTest\\Deploy' => 'LaminasTest\\Deploy',
+        'ZFTest\\DevelopmentMode' => 'LaminasTest\\DevelopmentMode',
+        'ZFTest\\Apigility' => 'ApigilityTest',
+        'ZFTest\\' => 'ApigilityTest',
+        'ZendServiceTest' => 'LaminasTest',
+        'zendframework.github.io' => 'docs.laminas.dev',
         'Zend Technologies USA, Inc.' => 'Laminas',
+        'Zend Technologies USA Inc.' => 'Laminas',
         'www.zend.com' => 'laminas.dev',
         'zendframework.com' => 'laminas.dev',
         'zendframework' => 'laminas',
         'zend' => 'laminas',
+        'zf2' => 'laminas',
         'zf' => 'laminas',
-        'Zend Technologies USA Inc.' => 'Laminas',
         'Zend Framework' => 'Laminas',
         'ZendFramework' => 'Laminas',
         'Zend' => 'Laminas',
-        'ZF' => 'Laminas',
+        'ZF' => 'Apigility',
     ];
 
     /** @var string */
@@ -45,6 +89,9 @@ class Repository
     {
         $this->name = $name;
         $this->path = getcwd();
+
+        $namespaceRwwrite = RewriteRules::namespaceRewrite();
+        $this->replacements = $namespaceRwwrite + $this->replacements;
     }
 
     public function getName() : string

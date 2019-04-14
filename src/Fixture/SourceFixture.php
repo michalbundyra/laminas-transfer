ca631994aef8f435eca749e3185f7de127b4b457
webimpress <contact@webimpress.com>
2019-04-14 19:06:07 +0200

    Add legacy function (with Zend) as proxy to new functions (with Laminas in name)
    Currently implement functionality requires PHP 5.6 in the library
    as it uses splat operator.
    It also checks files in 'src' directory, as we don't want to do it in
    tests.

--- src/Fixture/SourceFixture.php
+++ src/Fixture/SourceFixture.php
@@ -15,9 +15,15 @@ use function getcwd;
 use function implode;
 use function is_dir;
 use function mkdir;
+use function preg_match_all;
+use function str_ireplace;
 use function str_replace;
 use function strpos;
+use function substr;
 use function system;
+use function trim;
+
+use const PHP_EOL;
 
 /**
  * Change namespace and all references to ZF in source/test files
@@ -65,6 +71,42 @@ class SourceFixture extends AbstractFixture
     {
         $content = file_get_contents($file);
         $content = $repository->replace($content);
+        if (strpos($file, '/src/') !== false) {
+            $content = $this->deprecatedMethods($content);
+        }
         file_put_contents($file, $content);
+    }
+
+    /**
+     * Because of used splat operator it works only for libraries
+     * which supports PHP 5.6+.
+     */
+    private function deprecatedMethods(string $content) : string
+    {
+        if (substr(trim($content), -1) !== '}') {
+            return $content;
+        }
+
+        if (! preg_match_all('/^\s*public\s+(?<static>static\s+)?function\s+(?<name>[^(]*Laminas[^(]*)(?<params>\([^)]*\))/miU', $content, $matches)) {
+            return $content;
+        }
+
+        $deprecated = '';
+        foreach ($matches['name'] as $i => $name) {
+            $name = trim($name);
+            $legacy = str_ireplace('Laminas', 'Zend', $name);
+
+            $deprecated .= PHP_EOL . '    /**' . PHP_EOL
+                . '     * @deprecated Use self::' . $matches['name'][$i] . ' instead' . PHP_EOL
+                . '     */' . PHP_EOL
+                . '    public ' . $matches['static'][$i] . 'function ' . $legacy . $matches['params'][$i] . PHP_EOL
+                . '    {' . PHP_EOL
+                . '        return ' . ($matches['static'][$i] ? 'self::' : '$this->') . $name . '(...func_get_args());' . PHP_EOL
+                . '    }' . PHP_EOL;
+        }
+
+        return substr(trim($content), 0, -1)
+            . $deprecated
+            . '}' . PHP_EOL;
     }
 }

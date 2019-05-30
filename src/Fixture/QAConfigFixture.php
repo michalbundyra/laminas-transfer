fea6b7a2edba561b2559021ebcf76988ab6d8792
webimpress <contact@webimpress.com>
2019-05-30 19:38:32 +0100

    Improvements to QAConfig fixture
    Better .gitignore and .gitattributes rewrite

--- src/Fixture/QAConfigFixture.php
+++ src/Fixture/QAConfigFixture.php
@@ -9,11 +9,14 @@ use Laminas\Transfer\Repository;
 use function basename;
 use function current;
 use function explode;
+use function file_exists;
 use function file_get_contents;
 use function file_put_contents;
 use function implode;
 use function in_array;
+use function is_dir;
 use function sort;
+use function strstr;
 use function trim;
 
 class QAConfigFixture extends AbstractFixture
@@ -48,6 +51,68 @@ class QAConfigFixture extends AbstractFixture
         $filename = basename($file);
         if (in_array($filename, ['.gitattributes', '.gitignore'], true)) {
             $rows = explode("\n", $content);
+
+            if ($filename === '.gitattributes') {
+                foreach ($rows as $i => $row) {
+                    if (! $row) {
+                        continue;
+                    }
+
+                    $name = trim(strstr($row, ' ', true), '/');
+                    if (! file_exists($repository->getPath() . '/' . $name)) {
+                        unset($rows[$i]);
+                        continue;
+                    }
+
+                    $isDir = is_dir($repository->getPath() . '/' . $name);
+                    $rows[$i] = '/' . $name . ($isDir ? '/' : '') . ' export-ignore';
+                }
+            }
+
+            if ($filename === '.gitignore') {
+                foreach ($rows as $i => $row) {
+                    $line = trim($row);
+
+                    switch ($line) {
+                        case '.DS_STORE':
+                        case '.DS_Store':
+                        case '.*.sw*':
+                        case '.*.un~':
+                        case 'php-cs-fixer.phar':
+                        case '.buildpath':
+                        case '.buildpath/':
+                        case '/.idea':
+                        case '.idea':
+                        case '.idea/':
+                        case '/.project':
+                        case '.project':
+                        case '.project/':
+                        case '.settings':
+                        case '.settings/':
+                        case 'nbproject':
+                        case 'nbproject/':
+                        case 'tmp/':
+                        case 'composer.phar':
+                            unset($rows[$i]);
+                            break;
+                        case 'vendor':
+                        case 'doc/html':
+                        case 'docs/html':
+                            $rows[$i] = '/' . $line . '/';
+                            break;
+                        case 'vendor/':
+                        case 'doc/html/':
+                        case 'docs/html/':
+                        case 'phpunit.xml':
+                        case 'composer.lock':
+                        case 'clover.xml':
+                        case 'coveralls-upload.json':
+                            $rows[$i] = '/' . $line;
+                            break;
+                    }
+                }
+            }
+
             sort($rows);
             $content = trim(implode("\n", $rows)) . "\n";
         }

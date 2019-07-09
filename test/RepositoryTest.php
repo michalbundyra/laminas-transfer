e889e2e4063f3c0d4a9f5a04409421b3d84a9c06
webimpress <contact@webimpress.com>
2019-07-09 20:43:46 +0100

    Added PHPUnit configuration with basic tests

---
+++ test/RepositoryTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaminasTest;
+
+use Generator;
+use Laminas\Transfer\Repository;
+use PHPUnit\Framework\TestCase;
+
+class RepositoryTest extends TestCase
+{
+    /**
+     * @return string[]
+     */
+    public function name() : Generator
+    {
+        // ZendFramework
+        yield 'zendframework/zend-mvc' => ['zendframework/zend-mvc', 'laminas/laminas-mvc'];
+        yield 'zendframework/zendservice-twitter' => ['zendframework/zendservice-twitter', 'laminas/laminas-twitter'];
+
+        // Expressive
+        yield 'zendframework/zend-expressive' => ['zendframework/zend-expressive', 'expressive/expressive'];
+
+        // Apigility
+        yield 'zfcampus/zf-apigility' => ['zfcampus/zf-apigility', 'apigility/apigility'];
+        yield 'zfcampus/zf-apigility-admin' => ['zfcampus/zf-apigility-admin', 'apigility/apigility-admin'];
+        yield 'zfcampus/zf-hal' => ['zfcampus/zf-hal', 'apigility/apigility-hal'];
+    }
+
+    /**
+     * @dataProvider name
+     */
+    public function testNewName(string $legacyName, string $newName) : void
+    {
+        $repository = new Repository($legacyName);
+
+        self::assertSame($legacyName, $repository->getName());
+        self::assertSame($newName, $repository->getNewName());
+    }
+}

f4316f884154fde93c0f3ccdad2cf0ae2e6ad4a0
webimpress <contact@webimpress.com>
2019-07-21 16:00:40 +0100

    Remove license headers in test assets

--- test/Fixture/TestAsset/MiddlewareAttributes/ZendExpressiveHelpers/TemplateVariableContainerMiddleware.php
+++ test/Fixture/TestAsset/MiddlewareAttributes/ZendExpressiveHelpers/TemplateVariableContainerMiddleware.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * @see       https://github.com/zendframework/zend-expressive-helpers for the canonical source repository
- * @copyright Copyright (c) 2019 Zend Technologies USA Inc. (https://www.zend.com)
- * @license   https://github.com/zendframework/zend-expressive-helpers/blob/master/LICENSE.md New BSD License
- */
 
 declare(strict_types=1);
 

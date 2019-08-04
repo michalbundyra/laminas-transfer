c12cb9cde1f0e488cbdce03494afe6110d9ba81e
webimpress <contact@webimpress.com>
2019-08-04 18:56:23 +0200

    Composer fixture fixes and tests
    Sorting sections of composer.json
    Removing "minimum-stability" and "prefer-stable"
    Using https://apigility.org/documentation for apigility docs

--- src/Fixture/ComposerFixture.php
+++ src/Fixture/ComposerFixture.php
@@ -14,6 +14,7 @@ use Localheinz\Json\Normalizer\ChainNormalizer;
 use Localheinz\Json\Normalizer\Json;
 use Localheinz\Json\Normalizer\NormalizerInterface;
 
+use function array_search;
 use function array_unique;
 use function array_unshift;
 use function array_values;
@@ -22,6 +23,7 @@ use function explode;
 use function file_get_contents;
 use function json_decode;
 use function json_encode;
+use function uksort;
 use function unlink;
 
 /**
@@ -32,9 +34,33 @@ use function unlink;
  * Adds "laminas/laminas-zendframework-bridge" dependency to require section.
  * Updates license to BSD-3-Clause.
  * Prepends "laminas" and "expressive"/"apigility" keywords.
+ * Sorts sections as defined in constant.
+ * Removes redundant sections.
  */
 class ComposerFixture extends AbstractFixture
 {
+    private const SECTION_ORDER = [
+        'name',
+        'description',
+        'type',
+        'license',
+        'keywords',
+        'homepage',
+        'support',
+        'config',
+        'extra',
+        'require',
+        'require-dev',
+        'provide',
+        'conflict',
+        'suggest',
+        'autoload',
+        'autoload-dev',
+        'bin',
+        'scripts',
+        'replace',
+    ];
+
     public function process(Repository $repository) : void
     {
         $composerLock = current($repository->files('composer.lock'));
@@ -49,6 +75,7 @@ class ComposerFixture extends AbstractFixture
         }
 
         $content = file_get_contents($composer);
+        $originName = json_decode($content, true)['name'] ?? null;
         $content = $repository->replace($content);
 
         [$org, $name] = explode('/', $repository->getNewName(), 2);
@@ -60,8 +87,15 @@ class ComposerFixture extends AbstractFixture
             unset($json['type']);
         }
 
-        // Remove the authors, version, readme, time
-        unset($json['authors'], $json['version'], $json['readme'], $json['time']);
+        // Remove some redundant sections
+        unset(
+            $json['authors'],
+            $json['version'],
+            $json['readme'],
+            $json['time'],
+            $json['minimum-stability'],
+            $json['prefer-stable']
+        );
 
         $json['homepage'] = 'https://' . $org . '.dev';
 
@@ -79,17 +113,41 @@ class ComposerFixture extends AbstractFixture
         ];
 
         if ($org === 'apigility') {
-            unset($json['support']['docs']);
+            $json['support']['docs'] = 'https://apigility.org/documentation';
         }
 
         $json['require']['laminas/laminas-zendframework-bridge'] = '^0.3 || ^1.0';
         $json['require-dev']['roave/security-advisories'] = 'dev-master';
-        $json['replace'] = [$repository->getName() => 'self.version'];
+        $json['replace'] = [$originName ?? $repository->getName() => 'self.version'];
         if (isset($json['keywords'])) {
             array_unshift($json['keywords'], 'laminas', $org);
             $json['keywords'] = array_values(array_unique($json['keywords']));
         }
         $json['license'] = 'BSD-3-Clause';
+
+        // Sorting sections
+        uksort($json, static function (string $a, string $b) {
+            $ia = array_search($a, self::SECTION_ORDER, true);
+            $ib = array_search($b, self::SECTION_ORDER, true);
+
+            if ($ia === $ib) {
+                return 0;
+            }
+
+            if ($ia === false) {
+                return 1;
+            }
+
+            if ($ib === false) {
+                return -1;
+            }
+
+            if ($ia < $ib) {
+                return -1;
+            }
+
+            return 1;
+        });
 
         $json = Json::fromEncoded(json_encode($json));
         $normalized = $this->getNormalizer()->normalize($json);

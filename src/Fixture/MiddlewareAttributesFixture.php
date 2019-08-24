b155d1d5afb8604fc98d84b8424b5f22f13b54ca
webimpress <contact@webimpress.com>
2019-08-24 11:52:19 +0100

    MiddlewareAttribure fixture rewrite also middleware tests
    While mocking we need to set also legacy attribute name.

--- src/Fixture/MiddlewareAttributesFixture.php
+++ src/Fixture/MiddlewareAttributesFixture.php
@@ -9,39 +9,85 @@ use Laminas\Transfer\Repository;
 
 use function file_get_contents;
 use function file_put_contents;
+use function ltrim;
 use function preg_match_all;
 use function str_replace;
+use function strtr;
+
+use const PHP_EOL;
 
 class MiddlewareAttributesFixture extends AbstractFixture
 {
     public function process(Repository $repository) : void
     {
         $files = $repository->files('*/*Middleware.php');
-
         foreach ($files as $file) {
-            $content = file_get_contents($file);
+            $this->processMiddleware($repository, $file);
+        }
+        $repository->addReplacedContentFiles($files);
 
-            $namespace = NamespaceResolver::getNamespace($content);
-            $uses = NamespaceResolver::getUses($content);
+        $files = $repository->files('*/*MiddlewareTest.php');
+        foreach ($files as $file) {
+            $this->processMiddlewareTest($repository, $file);
+        }
+        $repository->addReplacedContentFiles($files);
+    }
 
-            $content = $repository->replace($content);
+    private function processMiddleware(Repository $repository, string $file) : void
+    {
+        $content = file_get_contents($file);
 
-            if (preg_match_all(
-                '/(\s*->withAttribute\(\s*)([^:$\'")]+::class)(,\s*)([^)]+)(\s*\))/',
-                $content,
-                $matches
-            )) {
-                foreach ($matches[2] as $i => $class) {
-                    $legacyName = '\\' . NamespaceResolver::getLegacyName($class, $namespace, $uses);
-                    $replace = $matches[0][$i] . $matches[1][$i]
-                        . $legacyName . $matches[3][$i] . $matches[4][$i] . $matches[5][$i];
-                    $content = str_replace($matches[0][$i], $replace, $content);
-                }
+        $namespace = NamespaceResolver::getNamespace($content);
+        $uses = NamespaceResolver::getUses($content);
+
+        $content = $repository->replace($content);
+
+        if (preg_match_all(
+            '/(\s*->withAttribute\(\s*)([^:$\'")]+::class)(,\s*)([^)]+)(\s*\))/',
+            $content,
+            $matches
+        )) {
+            foreach ($matches[2] as $i => $class) {
+                $legacyName = '\\' . NamespaceResolver::getLegacyName($class, $namespace, $uses);
+                $replace = $matches[0][$i] . $matches[1][$i]
+                    . $legacyName . $matches[3][$i] . $matches[4][$i] . $matches[5][$i];
+                $content = str_replace($matches[0][$i], $replace, $content);
             }
-
-            file_put_contents($file, $content);
         }
 
-        $repository->addReplacedContentFiles($files);
+        file_put_contents($file, $content);
+    }
+
+    private function processMiddlewareTest(Repository $repository, string $file) : void
+    {
+        $content = file_get_contents($file);
+
+        $namespace = NamespaceResolver::getNamespace($content);
+        $uses = NamespaceResolver::getUses($content);
+
+        $content = $repository->replace($content);
+
+        if (preg_match_all(
+            '/(?<before>\s*\$[a-z>\s-]*->withAttribute\(\s*)(?<name>[^$)\'"]+?)(?<after>\s*,[^;]*?;)/m',
+            $content,
+            $matches
+        )) {
+            $pairs = [];
+            foreach ($matches['name'] as $i => $class) {
+                $legacyName = NamespaceResolver::getLegacyName($class, $namespace, $uses);
+                if ($legacyName === $repository->replace($legacyName)) {
+                    continue;
+                }
+
+                $replace = $matches[0][$i] . PHP_EOL
+                    . ltrim($matches['before'][$i], "\n") . '\\' . $legacyName . $matches['after'][$i];
+
+                $pairs[$matches[0][$i]] = $replace;
+            }
+
+            $content = strtr($content, $pairs);
+        }
+
+        file_put_contents($file, $content);
     }
 }
---
+++ test/Fixture/TestAsset/MiddlewareAttributes/ZendExpressiveAuthentication/test/AuthenticationMiddlewareTest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ZendTest\Expressive\Authentication;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Zend\Expressive\Authentication\AuthenticationInterface;
+use Zend\Expressive\Authentication\AuthenticationMiddleware;
+use Zend\Expressive\Authentication\UserInterface;
+
+class AuthenticationMiddlewareTest extends TestCase
+{
+    protected $authentication;
+    protected $request;
+
+    public function setUp()
+    {
+        $this->authentication = $this->prophesize(AuthenticationInterface::class);
+        $this->request = $this->prophesize(ServerRequestInterface::class);
+        $this->authenticatedUser = $this->prophesize(UserInterface::class);
+        $this->handler = $this->prophesize(RequestHandlerInterface::class);
+    }
+
+    public function testConstructor()
+    {
+        $middleware = new AuthenticationMiddleware($this->authentication->reveal());
+        $this->assertInstanceOf(AuthenticationMiddleware::class, $middleware);
+        $this->assertInstanceOf(MiddlewareInterface::class, $middleware);
+    }
+
+    public function testProcessWithNoAuthenticatedUser()
+    {
+        $response = $this->prophesize(ResponseInterface::class);
+
+        $this->authentication->authenticate($this->request->reveal())
+                             ->willReturn(null);
+        $this->authentication->unauthorizedResponse($this->request->reveal())
+                             ->willReturn($response->reveal());
+
+        $middleware = new AuthenticationMiddleware($this->authentication->reveal());
+        $result = $middleware->process($this->request->reveal(), $this->handler->reveal());
+
+        $this->assertInstanceOf(ResponseInterface::class, $result);
+        $this->assertEquals($response->reveal(), $result);
+        $this->authentication->unauthorizedResponse($this->request->reveal())->shouldBeCalled();
+    }
+
+    public function testProcessWithAuthenticatedUser()
+    {
+        $response = $this->prophesize(ResponseInterface::class);
+
+        $this->request->withAttribute(UserInterface::class, $this->authenticatedUser->reveal())
+                      ->willReturn($this->request->reveal());
+        $this->authentication->authenticate($this->request->reveal())
+                             ->willReturn($this->authenticatedUser->reveal());
+        $this->handler->handle($this->request->reveal())
+                      ->willReturn($response->reveal());
+
+        $middleware = new AuthenticationMiddleware($this->authentication->reveal());
+        $result = $middleware->process($this->request->reveal(), $this->handler->reveal());
+
+        $this->assertInstanceOf(ResponseInterface::class, $result);
+        $this->assertEquals($response->reveal(), $result);
+        $this->handler->handle($this->request->reveal())->shouldBeCalled();
+    }
+}

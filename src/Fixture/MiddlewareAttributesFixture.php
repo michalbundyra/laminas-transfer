6e6d5ca122aa64010367526464826857263e219f
webimpress <contact@webimpress.com>
2019-08-24 22:21:15 +0100

    MiddlewareAttribute fixture - added another case
    Better way of recognising the end of statement than regexp

--- src/Fixture/MiddlewareAttributesFixture.php
+++ src/Fixture/MiddlewareAttributesFixture.php
@@ -4,15 +4,24 @@ declare(strict_types=1);
 
 namespace Laminas\Transfer\Fixture;
 
+use Exception;
 use Laminas\Transfer\Helper\NamespaceResolver;
 use Laminas\Transfer\Repository;
 
+use function array_pop;
+use function array_shift;
+use function end;
+use function explode;
 use function file_get_contents;
 use function file_put_contents;
+use function implode;
+use function in_array;
 use function ltrim;
+use function md5;
 use function preg_match_all;
 use function str_replace;
-use function strtr;
+use function strlen;
+use function substr;
 
 use const PHP_EOL;
 
@@ -72,22 +81,99 @@ class MiddlewareAttributesFixture extends AbstractFixture
             $content,
             $matches
         )) {
-            $pairs = [];
-            foreach ($matches['name'] as $i => $class) {
-                $legacyName = NamespaceResolver::getLegacyName($class, $namespace, $uses);
+            $results = $this->updateToEndOfStatement($matches, $content);
+            foreach ($results as $row) {
+                $legacyName = NamespaceResolver::getLegacyName($row['name'], $namespace, $uses);
                 if ($legacyName === $repository->replace($legacyName)) {
                     continue;
                 }
 
-                $replace = $matches[0][$i] . PHP_EOL
-                    . ltrim($matches['before'][$i], "\n") . '\\' . $legacyName . $matches['after'][$i];
+                $search = implode('', $row);
+                $replace = $search . PHP_EOL
+                    . ltrim($row['before'], "\n") . '\\' . $legacyName . $row['after'];
 
-                $pairs[$matches[0][$i]] = $replace;
+                $content = str_replace($search, $replace, $content);
             }
-
-            $content = strtr($content, $pairs);
         }
 
         file_put_contents($file, $content);
+    }
+
+    private function updateToEndOfStatement(array $matches, string $content) : array
+    {
+        $result = [];
+
+        foreach ($matches['before'] as $i => $before) {
+            $before = ltrim($before, "\n");
+
+            $delimiter = $before . $matches['name'][$i];
+            $exp = explode($delimiter, $content);
+            array_shift($exp);
+
+            foreach ($exp as $part) {
+                $end = $this->getEndOfStatementPosition($part, ['(']);
+                $after = substr($part, 0, $end + 1);
+
+                $hash = md5($delimiter . $after);
+
+                $result[$hash] = [
+                    'before' => $before,
+                    'name' => $matches['name'][$i],
+                    'after' => $after,
+                ];
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function getEndOfStatementPosition(string $string, array $stack) : int
+    {
+        $max = strlen($string);
+        for ($n = 0; $n < $max; ++$n) {
+            $last = end($stack);
+            switch ($string[$n]) {
+                case '(':
+                case '{':
+                case '[':
+                    if (! in_array($last, ['\'', '""'], true)) {
+                        $stack[] = $string[$n];
+                    }
+                    break;
+                case '\'':
+                case '"':
+                    if ($last === $string[$n]) {
+                        array_pop($stack);
+                    } elseif (! in_array($last, ['\'', '"'], true)) {
+                        $stack[] = $string[$n];
+                    }
+                    break;
+                case ')':
+                    if ($last === '(') {
+                        array_pop($stack);
+                    }
+                    break;
+                case '}':
+                    if ($last === '{') {
+                        array_pop($stack);
+                    }
+                    break;
+                case ']':
+                    if ($last === '[') {
+                        array_pop($stack);
+                    }
+                    break;
+                case ';':
+                    if (! $stack) {
+                        return $n;
+                    }
+                    break;
+            }
+        }
+
+        throw new Exception('Cannot find end of the statement!');
     }
 }

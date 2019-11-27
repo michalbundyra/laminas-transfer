eb204b36e446864172d0a1e34109d5f2a22f1ce7
Michał Bundyra <contact@webimpress.com>
2019-11-27 14:30:32 +0000

    Skip packages without `/` in name (like `php` or `ext-*`)

--- src/Fixture/LocalFixture.php
+++ src/Fixture/LocalFixture.php
@@ -20,6 +20,7 @@ use function file_get_contents;
 use function in_array;
 use function json_decode;
 use function json_encode;
+use function strpos;
 
 class LocalFixture extends AbstractFixture
 {
@@ -57,6 +58,10 @@ class LocalFixture extends AbstractFixture
 
     private function addLocalRepository(string $section, string $repo, array &$json) : void
     {
+        if (strpos($repo, '/') === false) {
+            return;
+        }
+
         [$org, $name] = explode('/', $repo);
 
         if ($name === 'laminas-zendframework-bridge'

15e6bdf7d1be2af50a0f8b7ff86dda101a9a11c4
Michał Bundyra <contact@webimpress.com>
2019-12-16 07:43:22 +0100

    Custom fixture for ZendService_Twitter
    Rename xml tests assets

---
+++ src/Fixture/Custom/ZendserviceTwitter.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Laminas\Transfer\Fixture\Custom;
+
+use Laminas\Transfer\Fixture\AbstractFixture;
+use Laminas\Transfer\Repository;
+
+use function file_get_contents;
+use function file_put_contents;
+use function str_replace;
+
+/**
+ * Process *.txt/*.php files (test assets)
+ * Rename ZF\d+ files to Laminas\d+
+ */
+class ZendserviceTwitter extends AbstractFixture
+{
+    public function process(Repository $repository) : void
+    {
+        $files = $repository->files('*.xml');
+
+        foreach ($files as $file) {
+            $content = file_get_contents($file);
+            $content = $repository->replace($content);
+            file_put_contents($file, $content);
+
+            $newName = str_replace('ZendService/Twitter/_files', 'Laminas/Twitter/_files', $file);
+
+            if ($newName !== $file) {
+                $repository->move($file, $newName);
+            }
+        }
+
+        $repository->addReplacedContentFiles($files);
+    }
+}

b039e8016f8d7080e461bb19abb645be0f4dcd53
Michał Bundyra <contact@webimpress.com>
2019-12-17 17:54:20 +0100

    Hotfix: custom fixture for expressive-skeleton to handle Twig templates
    We don't need to check for twig files in global Source fixture.

---
+++ src/Fixture/Custom/ZendExpressiveSkeleton.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Laminas\Transfer\Fixture\Custom;
+
+use Laminas\Transfer\Fixture\AbstractFixture;
+use Laminas\Transfer\Repository;
+
+use function file_get_contents;
+use function file_put_contents;
+use function getcwd;
+use function strtr;
+
+/**
+ * Process *.txt/*.php files (test assets)
+ * Rename ZF\d+ files to Laminas\d+
+ */
+class ZendExpressiveSkeleton extends AbstractFixture
+{
+    public function process(Repository $repository) : void
+    {
+        $files = $repository->files('*.twig');
+
+        foreach ($files as $file) {
+            $content = file_get_contents($file);
+            $content = $repository->replace($content);
+            file_put_contents($file, $content);
+
+            $newName = strtr($file, [
+                getcwd() => getcwd(),
+                'Expressive' => 'Mezzio',
+            ]);
+
+            if ($newName !== $file) {
+                $repository->move($file, $newName);
+            }
+        }
+
+        $repository->addReplacedContentFiles($files);
+    }
+}
